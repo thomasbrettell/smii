020803882174be6a30600fd7c4e59855f1fbc075
Brettell <thomas.a.brettell@gmail.com>
2024-12-04 19:17:43 +1100

    smile controls

---
+++ src/lib/Anchor.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import type { Writable } from "svelte/store";
+
+  export let point: Writable<number[]>;
+  export let r: number;
+  export let controlledPointRef: Writable<number[]> | null;
+  export let mouseDown: boolean;
+</script>
+
+<circle
+  cx={$point[0]}
+  cy={$point[1]}
+  {r}
+  fill="red"
+  class="anchor"
+  on:mousedown={() => {
+    controlledPointRef = point;
+    mouseDown = true;
+  }}
+  role="button"
+  tabindex={-1}
+/>
+
+<style>
+  circle {
+    cursor: pointer;
+    outline: none;
+  }
+</style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  export let hideControls: boolean;
+</script>
+
+<header>
+  <div>smii</div>
+  <div class="controls">
+    <label>
+      <input bind:checked={hideControls} type="checkbox" />
+      <span> hide controls </span>
+    </label>
+    <button>download</button>
+    <button>randomise</button>
+  </div>
+</header>
+
+<style>
+  header {
+    position: fixed;
+    display: flex;
+    top: 0;
+    padding: 10px;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  .controls {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+
+  label {
+    display: flex;
+    align-items: center;
+  }
+</style>
